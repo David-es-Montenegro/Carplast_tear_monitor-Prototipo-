96c0a33788e0efb75955a72eded4329cdb4b6734
David-es-Montenegro <115959360+David-es-Montenegro@users.noreply.github.com>
2023-03-07 00:20:52 -0300

    Update Carplast_tear_monitor.ino

--- Carplast_tear_monitor.ino
+++ Carplast_tear_monitor.ino
@@ -1,9 +1,8 @@
-#define pino1 16 //D0
-#define pino2 5  //D1
+const int pinos[]{ 16, 5, 4, 2, 14, 12, 13 };  //D0 D1 D2 D4 D5 D6 D7
 
-const unsigned int paradas[6] = {100000, 110000, 000001, 100001,010001, 000100};
+const String paradas[6] = { "1100000", "1110000", "1000001", "1100001", "1010001", "1000100" };
 unsigned int qntParadas[6];
-float tempoParadas[6], eficienciaTotal = 0; perdasEficienciaParadas[6];
+float tempoParadas[6], eficienciaTotal = 0, perdasEficienciaParadas[6], tempoMedio[6];
 unsigned long tempoInicial, tempoFinal, decorrido = 0, horaDeLigacao = 0, tempoParado = 0, tempoLigado = 0;
 
 
@@ -21,93 +20,16 @@ void setup() {
 
 void loop() {
 
-  
+
 
   if (analogRead(13) != 0) {
     mostrador();
   }
 
-  if (leitura() != "000000") {
-    delay(100);
-    if (leitura() == parada1) {
-      tempoInicial = millis();
-      countParada1 = countParada1 + 1;
+  comparador();
 
-      while (leitura() == parada1) {
-        delay(10);
-        tempoFinal = millis();
-      }
 
-      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
-      tempoParada1 = tempoParada1 + decorrido;
-      decorrido = 0;
-    }
-    if (leitura() == parada2) {
-      tempoInicial = millis();
-      countParada2 = countParada2 + 1;
 
-      while (leitura() == parada2) {
-        delay(10);
-        tempoFinal = millis();
-      }
-
-      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
-      tempoParada2 = tempoParada2 + decorrido;
-      decorrido = 0;
-    }
-    if (leitura() == parada3) {
-      tempoInicial = millis();
-      countParada3 = countParada3 + 1;
-
-      while (leitura() == parada3) {
-        delay(10);
-        tempoFinal = millis();
-      }
-
-      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
-      tempoParada3 = tempoParada3 + decorrido;
-      decorrido = 0;
-    }
-    if (leitura() == parada4) {
-      tempoInicial = millis();
-      countParada4 = countParada4 + 1;
-
-      while (leitura() == parada4) {
-        delay(10);
-        tempoFinal = millis();
-      }
-
-      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
-      tempoParada4 = tempoParada4 + decorrido;
-      decorrido = 0;
-    }
-    if (leitura() == parada5) {
-      tempoInicial = millis();
-      countParada5 = countParada5 + 1;
-
-      while (leitura() == parada5) {
-        delay(10);
-        tempoFinal = millis();
-      }
-
-      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
-      tempoParada5 = tempoParada5 + decorrido;
-      decorrido = 0;
-    }
-    if (leitura() == parada6) {
-      tempoInicial = millis();
-      countParada6 = countParada6 + 1;
-
-      while (leitura() == parada6) {
-        delay(10);
-        tempoFinal = millis();
-      }
-
-      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
-      tempoParada6 = tempoParada6 + decorrido;
-      decorrido = 0;
-    }
-  }
 
 
   delay(10);
@@ -115,40 +37,121 @@ void loop() {
 }
 
 String leitura() {
-  String sequencia;
+  String sequencia = "1";
 
-  sequencia = sequencia + digitalRead(16);
-  sequencia = sequencia + digitalRead(5);
-  sequencia = sequencia + digitalRead(4);
-  sequencia = sequencia + digitalRead(12);
-  sequencia = sequencia + digitalRead(2);
-  sequencia = sequencia + digitalRead(14);
-
+  sequencia = sequencia + digitalRead(pinos[0]);
+  sequencia = sequencia + digitalRead(pinos[1]);
+  sequencia = sequencia + digitalRead(pinos[2]);
+  sequencia = sequencia + digitalRead(pinos[3]);
+  sequencia = sequencia + digitalRead(pinos[4]);
+  sequencia = sequencia + digitalRead(pinos[5]);
 
   return sequencia;
 }
 
-String armazenador() {
-  String parada;
-  parada = leitura();
-  return parada;
-}
-
 void mostrador() {
-  calculadora();
+  calculador();
   Serial.println(eficienciaTotal);
   Serial.println(tempoLigado);
   Serial.println(tempoParado);
-  Serial.println("parada1: " + parada1 + " paradas: " + String(countParada1) + " tempo total: " + String(tempoParada1) + " tempo medio: " + String((tempoParada1 / countParada1)));
-  Serial.println("parada2: " + parada2 + " paradas: " + String(countParada2) + " tempo total: " + String(tempoParada2) + " tempo medio: " + String((tempoParada2 / countParada2)));
-  Serial.println("parada3: " + parada3 + " paradas: " + String(countParada3) + " tempo total: " + String(tempoParada3) + " tempo medio: " + String((tempoParada3 / countParada3)));
-  Serial.println("parada4: " + parada4 + " paradas: " + String(countParada4) + " tempo total: " + String(tempoParada4) + " tempo medio: " + String((tempoParada4 / countParada4)));
-  Serial.println("parada5: " + parada5 + " paradas: " + String(countParada5) + " tempo total: " + String(tempoParada5) + " tempo medio: " + String((tempoParada5 / countParada5)));
-  Serial.println("parada6: " + parada6 + " paradas: " + String(countParada6) + " tempo total: " + String(tempoParada6) + " tempo medio: " + String((tempoParada6 / countParada6)));
+
+  for (int i = 0; i <= 5; i++) {
+    String codParada = paradas[i];
+    codParada.remove(0,1);
+    Serial.println("parada" + String((i + 1)) + ": " + codParada + " paradas: " + String(qntParadas[i]) + " tempo total: " + String(tempoParadas[i]) + " tempo medio: " + String(tempoMedio[i]));
+  }
 }
 
-void calculadora (){
+void calculador() {
   tempoLigado = ((millis() / 1000) - (horaDeLigacao / 1000));
-  tempoParado = (tempoParada1 + tempoParada2 + tempoParada3 + tempoParada4 + tempoParada5 + tempoParada6);
+
+  for (int i = 0; i < 6; i++) {
+    tempoParado = tempoParado + tempoParadas[i];
+  }
+
   eficienciaTotal = ((tempoParado * 100) / tempoLigado);
+}
+
+void comparador() {
+  if (leitura() != "000000") {
+    delay(100);
+    if (leitura() == paradas[0]) {
+      tempoInicial = millis();
+      qntParadas[0] = qntParadas[0] + 1;
+
+      while (leitura() == paradas[0]) {
+        delay(10);
+        tempoFinal = millis();
+      }
+
+      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
+      tempoParadas[0] = tempoParadas[0] + decorrido;
+      decorrido = 0;
+    }
+    if (leitura() == paradas[1]) {
+      tempoInicial = millis();
+      qntParadas[1] = qntParadas[1] + 1;
+
+      while (leitura() == paradas[1]) {
+        delay(10);
+        tempoFinal = millis();
+      }
+
+      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
+      tempoParadas[1] = tempoParadas[1] + decorrido;
+      decorrido = 0;
+    }
+    if (leitura() == paradas[2]) {
+      tempoInicial = millis();
+      qntParadas[2] = qntParadas[2] + 1;
+
+      while (leitura() == paradas[2]) {
+        delay(10);
+        tempoFinal = millis();
+      }
+
+      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
+      tempoParadas[2] = tempoParadas[2] + decorrido;
+      decorrido = 0;
+    }
+    if (leitura() == paradas[3]) {
+      tempoInicial = millis();
+      qntParadas[3] = qntParadas[3] + 1;
+
+      while (leitura() == paradas[3]) {
+        delay(10);
+        tempoFinal = millis();
+      }
+
+      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
+      tempoParadas[3] = tempoParadas[3] + decorrido;
+      decorrido = 0;
+    }
+    if (leitura() == paradas[4]) {
+      tempoInicial = millis();
+      qntParadas[4] = qntParadas[4] + 1;
+
+      while (leitura() == paradas[4]) {
+        delay(10);
+        tempoFinal = millis();
+      }
+
+      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
+      tempoParadas[4] = tempoParadas[4] + decorrido;
+      decorrido = 0;
+    }
+    if (leitura() == paradas[5]) {
+      tempoInicial = millis();
+      qntParadas[5] = qntParadas[5] + 1;
+
+      while (leitura() == paradas[5]) {
+        delay(10);
+        tempoFinal = millis();
+      }
+
+      decorrido = decorrido + ((tempoFinal / 1000) - (tempoInicial / 1000));
+      tempoParadas[5] = tempoParadas[5] + decorrido;
+      decorrido = 0;
+    }
+  }
 }

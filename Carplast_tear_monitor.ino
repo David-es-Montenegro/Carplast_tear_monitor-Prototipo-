7b560a497b3f15a10385218038ff7f5ef48d64fd
David-es-Montenegro <115959360+David-es-Montenegro@users.noreply.github.com>
2023-03-10 00:29:41 -0300

    Update Carplast_tear_monitor
    Comentarios adicionados.

--- Carplast_tear_monitor.ino
+++ Carplast_tear_monitor.ino
@@ -1,9 +1,10 @@
 const int pinos[]{ 16, 5, 4, 2, 14, 12, 13 };  //D0 D1 D2 D4 D5 D6 D7
 
-const String paradas[6] = { "1100000", "1110000", "1000001", "1100001", "1010001", "1000100" };
+const String paradas[6] = { "1100000", "1110000", "1000001", "1100001", "1010001", "1000100" }; // Modelo: "1000100" cada constante deve iniciar com o digito 1, caso contrario, os caracteres antes de algum digito "1" serão descartados,
+//os proximos 6 digitos significam "1" = sensor ativado, "0" sensor desativado, cada tipo de parada do tear é representado por uma sequencia de 6 sensores ativados ou desativados.
+
 unsigned int qntParadas[6];
 float tempoParadas[6], eficienciaTotal = 0, perdasEficienciaParadas[6], tempoMedio[6], tempoParado = 0, tempoLigado = 0;
-;
 unsigned long tempoInicial, tempoFinal, decorrido = 0, horaDeLigacao = 0;
 
 
@@ -15,7 +16,7 @@ void setup() {
     pinMode(pinos[i], INPUT);
   }
 
-  horaDeLigacao = (millis() / 1000);
+  horaDeLigacao = (millis() / 1000); //captura milissegundos decorridos antes do loop para calcular o tempo ligado.
 }
 
 void loop() {
@@ -28,6 +29,7 @@ void loop() {
   yield();
 }
 
+//lê as o estado de cada sensor. (leitura = "1000000" == maquina em funcionamento pleno)
 String leitura() {
   String sequencia = "1";
 
@@ -38,6 +40,7 @@ String leitura() {
   return sequencia;
 }
 
+//exibe via serial monitor as metricas obtidas.
 void mostrador() {
   calculador();
 
@@ -54,11 +57,12 @@ void mostrador() {
   for (int i = 0; i < 6; i++) {
     String codParada = paradas[i];
     codParada.remove(0, 1);
-    Serial.println("parada" + String((i + 1)) + ": " + codParada + " paradas: " + String(qntParadas[i]) + " tempo total: " + String(tempoParadas[i]) + " tempo medio: " + String(tempoMedio[i]) + " perda de eficiencia causada: " + perdasEficienciaParadas[i] + "%");
+    Serial.println("parada" + String((i + 1)) + ": " + codParada + " paradas: " + String(qntParadas[i]) + " tempo total: " + String(tempoParadas[i]) + " tempo medio: " + String(tempoMedio[i]) + " perda de eficiencia causada: " + perdasEficienciaParadas[i] + "\%");
   }
   Serial.println("==========================================================================================================================================================================================================================");
 }
 
+//calcula todas as eficiencias e parametros.
 void calculador() {
   tempoParado = 0;
   tempoLigado = 0;
@@ -81,6 +85,7 @@ void calculador() {
   }
 }
 
+//conta o tempo de cada tipo de parada assim que um sensor sinaliza.
 void comparador() {
   if (leitura() != "000000") {
     delay(100);
